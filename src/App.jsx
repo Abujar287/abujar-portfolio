a8fd1f5b1f53046011a84a5add4ae70aa9d53f20
Abujar287 <abujar287.algifari@gmail.com>
2026-09-13 12:14:15 +0600

    Replace div with img for profile picture

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <main className="hero">
-      <div className="photo">AA</div>
+      <img src="/profile.jpg" alt="Abujar Al-Gifari" />
       <h1>ABUJAR AL-GIFARI</h1>
       <p>DATA ANALYST | BUSINESS INTELLIGENCE | EXCEL, SQL & PYTHON | CRM & CLM</p>
     </main>
